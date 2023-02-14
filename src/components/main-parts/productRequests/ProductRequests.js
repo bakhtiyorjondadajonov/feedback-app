0fac1b331ea679802c61092acb558698b9b9e87f
bakhtiyorjondadajonov <bakhtiyorjondadajonov@gmail.com>
2023-02-14 17:44:03 +0900

    product request card small bugs solved

--- src/components/main-parts/productRequests/ProductRequests.js
+++ src/components/main-parts/productRequests/ProductRequests.js
@@ -37,6 +37,7 @@ letter-spacing: -0.25px;
 text-align: left;
 }
 .paragraph{
+  cursor: pointer;
   grid-column:2/3;
   grid-row: 2/3;
   color: var(--color-primary);
@@ -62,7 +63,7 @@ function ProductRequestCard({productRequest}) {
   return (
     <Card>
      <h3 className="title">{productRequest.title}</h3>
-     <p className="paragraph">{productRequest.description}</p>
+     <p onClick={()=>navigatorFn(`/feedback-details/${productRequest.id}`)} className="paragraph">{productRequest.description}</p>
     <ButtonMain onClick={()=> navigatorFn(`/feedback-details/${productRequest.id}`)} className="comments-btn" transparent >
     <svg width="18" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M2.62 16H1.346l.902-.91c.486-.491.79-1.13.872-1.823C1.036 11.887 0 9.89 0 7.794 0 3.928 3.52 0 9.03 0 14.87 0 18 3.615 18 7.455c0 3.866-3.164 7.478-8.97 7.478-1.017 0-2.078-.137-3.025-.388A4.705 4.705 0 012.62 16z" fill="#CDD2EE" fill-rule="nonzero"/></svg>
     <h3>{productRequest.comments?productRequest.comments.length:0}</h3>
